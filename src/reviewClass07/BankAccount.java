f8de872f5e4ecfc6ec0e1da29a4f645314a92dd1
doncevan <doncevan@gmail.com>
2023-05-05 10:08:42 -0400

    reviewClass07

---
+++ src/reviewClass07/BankAccount.java
@@ -0,0 +1,10 @@
+package reviewClass07;
+
+public class BankAccount {
+    String acccountHolder;
+    long accountNumber;
+    double accBalance;
+    int ssn;
+
+    static String bankName = "BOA";
+}
