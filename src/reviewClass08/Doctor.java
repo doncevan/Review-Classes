50c53d4f15808481df1e1232b00ec23a7bec5b3c
doncevan <doncevan@gmail.com>
2023-05-11 22:47:30 -0400

    reviewClass08

---
+++ src/reviewClass08/Doctor.java
@@ -0,0 +1,40 @@
+package reviewClass08;
+
+public class Doctor {
+
+    public String firstName, lastName;
+    protected String speciality;
+    int yearsOfExperience;
+    private double salary;
+    public static boolean degree;
+
+    Doctor(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /*
+        to have more than 1 constructor we need to change number of the parameters or type of the parameters
+     */
+    public Doctor(String firstName, String lastName, String speciality) {
+        this(firstName, lastName); // this() -> calling a current class constructor
+        this.speciality = speciality; // this. referring to current class instance variables
+    }
+
+    public void printInfo() {
+        System.out.println("Doctors name is " + firstName + " " + lastName + ". And speciality is " + speciality);
+    }
+
+    protected void treat() {
+        System.out.println("Doctors treat patiences");
+    }
+
+    void prescribeMedicine(String medicine) {
+        System.out.println("Doctor prescribed " + medicine);
+    }
+
+    private double getSalary() {
+        return salary;
+    }
+
+}
---
+++ src/reviewClass08/Hospital.java
@@ -0,0 +1,13 @@
+package reviewClass08;
+
+public class Hospital {
+    public static void main(String[] args) {
+        Doctor doc = new Doctor("John", "Doe");
+        doc.printInfo();
+
+        Doctor doc2 = new Doctor("Jane", "Smith", "Cardiologyst");
+        doc2.printInfo(); //public
+        doc2.treat(); //protected
+        doc2.prescribeMedicine("antibiotics"); //default
+    }
+}
