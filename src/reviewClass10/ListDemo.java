724af7da8ebcc0df661f443f68dab381b733380b
doncevan <doncevan@gmail.com>
2023-05-25 22:18:05 -0400

    reviewClass10

---
+++ src/reviewClass10/ListDemo.java
@@ -0,0 +1,13 @@
+package reviewClass10;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListDemo {
+    List<String> names = new LinkedList<>();
+    List<String> names1 = new ArrayList<>();
+
+
+}
---
+++ src/reviewClass10/MoveAble.java
@@ -0,0 +1,27 @@
+package reviewClass10;
+
+
+public interface MoveAble {
+    void move();
+}
+
+class Dog implements MoveAble {
+    @Override
+    public void move() {
+        System.out.println("Dog is moving");
+    }
+}
+
+class Car implements MoveAble {
+    @Override
+    public void move() {
+        System.out.println("Car is moving");
+    }
+}
+
+class Human implements MoveAble {
+    @Override
+    public void move() {
+        System.out.println("Human moving....");
+    }
+}
